296ce7190970d118e9b7efaff33373c4a8b93287
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2023-12-18 09:54:39 +0000

    adds leaflet maps to template

---
+++ R/00_libraries.R
@@ -0,0 +1,5 @@
+library(tidyverse) #data manipulation
+library(janitor) #clean_names
+library(sf) #import geography files (shapes)
+library(leaflet) #for mapping
+library(RColorBrewer) #for colour palette
