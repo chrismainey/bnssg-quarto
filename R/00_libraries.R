0b9ee592d72a8bcd1bfbdba61adda6f1f259b632
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2023-12-18 10:06:49 +0000

    adds collapsible tables and tabset plots to template

--- R/00_libraries.R
+++ R/00_libraries.R
@@ -3,3 +3,4 @@ library(janitor) #clean_names
 library(sf) #import geography files (shapes)
 library(leaflet) #for mapping
 library(RColorBrewer) #for colour palette
+library(gt) # for printing tables
